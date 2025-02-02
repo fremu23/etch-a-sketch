147a79102dd5bfbf7c83b5e643914d0eee7dd9c5
fremu23 <175006466+fremu23@users.noreply.github.com>
2025-02-02 20:14:46 +1300

    Add button to change grid size
    Using the JS event listener we are able to make button display a prompt
    which gives the user the option to choose the size of the grid.

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const col = 16;
 const containerSize = 500; // 500px in width and height
 const cellSize = containerSize / row;
 
-
 for (i=0; i < row*col;i++){
     const cell = document.createElement("div");
     cell.classList.add("cell"); 
@@ -16,8 +15,28 @@ for (i=0; i < row*col;i++){
         cell.style.backgroundColor = "lightblue";
     })
 
-
-
     gridContainer.appendChild(cell);
 }
 
+const changeGridSize = document.getElementById("grid-change-btn");
+
+changeGridSize.addEventListener("click", () => {
+    while (true) {
+        let numRows = prompt("How many rows in the grid? (1-100)");
+
+        if (numRows == null){
+            return;
+        }
+
+        numRows = parseInt(numRows);
+
+        if (numRows > 100 || numRows < 1){
+            alert("Please enter a number between 1 and 100.");
+        } else {
+            console.log(numRows);
+            break;
+            
+        }
+    }
+
+})
